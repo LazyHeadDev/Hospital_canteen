4be90f9daddabfac0781863e7f874727bd49374a
LazyHeadDev <156129442+LazyHeadDev@users.noreply.github.com>
2025-07-12 14:35:06 +0200

    Add files via upload

--- data.js
+++ data.js
@@ -24,19 +24,26 @@ var APP_DATA = {
       ],
       "faceSize": 2000,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": -0.010348184239882485,
+        "pitch": 0.10174509567796086,
+        "fov": 0.9250380511135141
       },
       "linkHotspots": [
         {
-          "yaw": -0.5893557291851224,
-          "pitch": 0.2608146529884152,
+          "yaw": 0.3297340976350327,
+          "pitch": 0.20680816828012283,
           "rotation": 0,
           "target": "1-hospital_canteen_360_01"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": -2.3246545423775693,
+          "pitch": 0.53588473281512,
+          "title": "Chaise \"Nuta\"",
+          "text": "Text"
+        }
+      ]
     },
     {
       "id": "1-hospital_canteen_360_01",
@@ -60,21 +67,79 @@ var APP_DATA = {
           "size": 2048
         }
       ],
-      "faceSize": 1500,
+      "faceSize": 2000,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 0.021502410765315005,
+        "pitch": 0.031803898560024635,
+        "fov": 1.020404613484628
       },
       "linkHotspots": [
         {
-          "yaw": 2.1909730832280063,
-          "pitch": 0.08270633213221146,
+          "yaw": -2.864078133775889,
+          "pitch": 0.21540096393999164,
           "rotation": 0,
           "target": "0-hospital_canteen_360"
+        },
+        {
+          "yaw": -0.6886551171317485,
+          "pitch": 0.15173729676789627,
+          "rotation": 0,
+          "target": "2-hospital_canteen_360_02"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": 0.7741624454963247,
+          "pitch": 0.2019490864535456,
+          "title": "<pre class=\"tw-data-text tw-text-large tw-ta\" data-placeholder=\"Translation\" id=\"tw-target-text\" data-ved=\"2ahUKEwiAk-aWp7eOAxXSfKQEHdLUNeYQ3ewLegQICRAV\" dir=\"ltr\" aria-label=\"Translated text: la porte s'ouvre vers l'extérieur\" style=\"font-size: 28px; line-height: 36px; background-color: rgb(48, 49, 52); border: none; padding: 2px 0.14em 2px 0px; position: relative; margin: -2px 0px; resize: none; font-family: inherit; overflow: hidden; width: 270px; text-wrap-mode: wrap; overflow-wrap: break-word; color: rgb(232, 234, 237);\"><br></pre>",
+          "text": "<pre class=\"tw-data-text tw-text-large tw-ta\" data-placeholder=\"Translation\" id=\"tw-target-text\" data-ved=\"2ahUKEwiAk-aWp7eOAxXSfKQEHdLUNeYQ3ewLegQICRAV\" dir=\"ltr\" aria-label=\"Translated text: la porte s'ouvre vers l'extérieur\" style=\"font-size: 28px; line-height: 36px; background-color: rgb(48, 49, 52); border: none; padding: 2px 0.14em 2px 0px; position: relative; margin: -2px 0px; resize: none; font-family: inherit; overflow: hidden; width: 270px; text-wrap-mode: wrap; overflow-wrap: break-word; color: rgb(232, 234, 237);\"><pre class=\"tw-data-text tw-text-large tw-ta\" data-placeholder=\"Translation\" id=\"tw-target-text\" data-ved=\"2ahUKEwiAk-aWp7eOAxXSfKQEHdLUNeYQ3ewLegQICRAV\" dir=\"ltr\" aria-label=\"Translated text: la porte s'ouvre vers l'extérieur\" style=\"font-size: 28px; line-height: 36px; border: none; padding: 2px 0.14em 2px 0px; position: relative; margin: -2px 0px; resize: none; font-family: inherit; overflow: hidden; width: 270px; text-wrap-mode: wrap; overflow-wrap: break-word;\"><span class=\"Y2IQFc\" lang=\"fr\">porte s'ouvre vers l'extérieur</span></pre></pre>"
+        }
+      ]
+    },
+    {
+      "id": "2-hospital_canteen_360_02",
+      "name": "Hospital_canteen_360_02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.16247564838555562,
+        "pitch": 0.1332141177760704,
+        "fov": 1.0693586371858816
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.085834677929437,
+          "pitch": 0.189279585971871,
+          "rotation": 0,
+          "target": "1-hospital_canteen_360_01"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.09723370103019668,
+          "pitch": 0.12050462252502925,
+          "title": "Plaque four 2 Bacs",
+          "text": "Text"
+        }
+      ]
     }
   ],
   "name": "Project Title",
